7360ae77189cab71fa3fb1a6f87e3e5bba6ae0f6
wangzm <1335130564@qq.com>
2025-11-16 13:46:01 +0800

    feat: 添加按年/月统计

---
+++ src/pages/statistics/components/time-demension.tsx
@@ -0,0 +1,75 @@
+import { StyleSheet } from 'react-native';
+import { SegmentedButtons, Surface } from 'react-native-paper';
+import dayjs from 'dayjs';
+
+interface TimeDemensionProps {
+	timeDimension: 'month' | 'year';
+    monthList: string[];
+	yearList: number[];
+	selectedMonth: string;
+	selectedYear: string;
+	setSelectedYear: (selectedYear: string) => void;
+	setSelectedMonth: (selectedMonth: string) => void;
+	setTimeDimension: (timeDimension: 'month' | 'year') => void;
+}
+
+function TimeDemension({
+	timeDimension,
+	monthList,
+	selectedMonth,
+	yearList,
+	selectedYear,
+	setSelectedYear,
+	setSelectedMonth,
+	setTimeDimension,
+}: TimeDemensionProps) {
+	return (
+		<Surface style={styles.dimensionContainer}>
+			<SegmentedButtons
+				value={timeDimension}
+				onValueChange={setTimeDimension}
+				buttons={[
+					{
+						value: 'month',
+						label: '月',
+					},
+					{
+						value: 'year',
+						label: '年',
+					},
+				]}
+			/>
+            {timeDimension === 'month' && (
+                <SegmentedButtons
+                    value={selectedMonth}
+                    onValueChange={(month: string) => setSelectedMonth(month)}
+                    buttons={monthList.map(month => ({
+                        value: month,
+                        label: dayjs(month).format('MM'),
+                    }))}
+                />
+            )}
+			{timeDimension === 'year' && (
+                <SegmentedButtons
+                    value={selectedYear.toString()}
+                    onValueChange={(year: string) => setSelectedYear(year)}
+                    buttons={yearList.map(year => ({
+                        value: year.toString(),
+                        label: year.toString(),
+                    }))}
+                />
+			)}
+		</Surface>
+	);
+}
+
+export default TimeDemension;
+
+const styles = StyleSheet.create({
+	dimensionContainer: {
+		justifyContent: 'center',
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		gap: 12,
+	},
+});
--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -3,6 +3,8 @@ import { useCategoryStore, useTransactionStore } from '../../store';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { useEffect, useMemo, useState } from 'react';
 import { Transaction } from '../../type';
+import TimeDemension from './components/time-demension';
+import dayjs from 'dayjs';
 
 type CategoryTransactions = Record<
 	string,
@@ -17,12 +19,21 @@ function Statistics() {
 	const { transactions } = useTransactionStore();
 	const { categories } = useCategoryStore();
 	const [categoryTransactions, setCategoryTransactions] = useState<CategoryTransactions>();
+	const [timeDimension, setTimeDimension] = useState<'month' | 'year'>('month');
+	const [monthList, setMonthList] = useState<string[]>([]);
+	const [yearList, setYearList] = useState<number[]>([]);
+	const [selectedMonth, setSelectedMonth] = useState<string>();
+	const [selectedYear, setSelectedYear] = useState<string>();
 
 	useEffect(() => {
 		// 按时间过滤数据
-		const filteredRecord = transactions.filter(transaction => {
-			return transaction;
-		});
+		let filteredRecord = transactions;
+
+		if (timeDimension === 'month' && selectedMonth) {
+			filteredRecord = filteredRecord.filter(record => dayjs(record.date).isSame(dayjs(selectedMonth), 'month'));
+		} else if (timeDimension === 'year' && selectedYear) {
+			filteredRecord = filteredRecord.filter(record => dayjs(record.date).year() === Number(selectedYear));
+		}
 
 		// 过滤后的数据分类
 		const map: CategoryTransactions = {};
@@ -31,7 +42,7 @@ function Statistics() {
 			if (vals) {
 				map[record.category] = { ...vals, total: vals.total + record.amount };
 			} else {
-				const catagory = categories.find(item => (item.name === record.category));
+				const catagory = categories.find(item => item.name === record.category);
 
 				map[record.category] = {
 					list: [record],
@@ -43,25 +54,76 @@ function Statistics() {
 
 		// 排序...
 		setCategoryTransactions(map);
-	}, [transactions, categories]);
+	}, [transactions, categories, timeDimension, selectedMonth, selectedYear]);
+
+	useEffect(() => {
+		if (transactions.length === 0) {
+			setMonthList([]);
+			setYearList([]);
+			setSelectedMonth(dayjs().format('YYYY-MM'));
+			setSelectedYear(dayjs().format('YYYY'));
+			return;
+		}
+
+		const firstTransaction = transactions[transactions.length - 1];
+
+		const currentYear = dayjs().year();
+
+		const firstYear = dayjs(firstTransaction.date).year();
+
+		const startMonth = dayjs(firstTransaction.date).startOf('month');
+		const endMonth = dayjs().startOf('month');
+		const _monthList: string[] = [];
+		let cursor = startMonth;
+		while (cursor.isBefore(endMonth) || cursor.isSame(endMonth)) {
+			_monthList.push(cursor.format('YYYY-MM'));
+			cursor = cursor.add(1, 'month');
+		}
+
+		const _yearList: number[] = [];
+		for (let year = firstYear; year <= currentYear; year++) {
+			_yearList.push(year);
+		}
+
+		setMonthList(_monthList);
+		setYearList(_yearList);
+		setSelectedMonth(dayjs().format('YYYY-MM'));
+		setSelectedYear(dayjs().format('YYYY'));
+	}, [transactions]);
 
 	const renderRecord = useMemo(() => {
 		if (!categoryTransactions) return null;
 
-		return Object.entries(categoryTransactions).map(([categoryName, record]) => (
-			<Surface key={categoryName} style={styles.recordItem}>
-				<Icon source={record.icon} size={24} />
-				<Text style={styles.categoryName}>{categoryName}</Text>
-				<View style={styles.recordDetails}>
-					<Text>总金额: ¥{record.total}</Text>
-					{/* <Text>交易笔数: {record.count}</Text> */}
-				</View>
-			</Surface>
-		));
+		return Object.entries(categoryTransactions)
+			.sort((a, b) => {
+				return b[1].total - a[1].total;
+			})
+			.map(([categoryName, record]) => (
+				<Surface key={categoryName} style={styles.recordItem}>
+					<View style={styles.recordItemContent}>
+						<Icon source={record.icon} size={24} />
+						<Text style={styles.categoryName}>{categoryName}</Text>
+						{/* <ProgressBar progress={0.5} /> */}
+					</View>
+					<View style={styles.recordDetails}>
+						<Text>总金额: ¥{record.total}</Text>
+					</View>
+				</Surface>
+			));
 	}, [categoryTransactions]);
 
 	return (
 		<View style={styles.container}>
+			<TimeDemension
+				setTimeDimension={setTimeDimension}
+				timeDimension={timeDimension}
+				monthList={monthList}
+				selectedMonth={selectedMonth || ''}
+				setSelectedMonth={setSelectedMonth}
+				yearList={yearList}
+				selectedYear={selectedYear || ''}
+				setSelectedYear={setSelectedYear}
+			/>
 			<ScrollView>
 				<View style={styles.record}>{renderRecord}</View>
 			</ScrollView>
@@ -91,5 +153,10 @@ const styles = StyleSheet.create({
 	},
 	recordDetails: {
 		alignItems: 'flex-end',
+	},
+	recordItemContent: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8,
 	},
 });
