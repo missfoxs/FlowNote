4815bbc249910ef9deb7540fa020d41ca2d9aa65
wangzm <1335130564@qq.com>
2025-11-16 09:49:19 +0800

    feat: 添加图标

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,6 +1,6 @@
-import { Surface, Text } from 'react-native-paper';
-import { useTransactionStore } from '../../store';
-import { StyleSheet, View } from 'react-native';
+import { Icon, Surface, Text } from 'react-native-paper';
+import { useCategoryStore, useTransactionStore } from '../../store';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { useEffect, useMemo, useState } from 'react';
 import { Transaction } from '../../type';
 
@@ -9,11 +9,13 @@ type CategoryTransactions = Record<
 	{
 		list: Transaction[];
 		total: number;
+		icon: string;
 	}
 >;
 
 function Statistics() {
 	const { transactions } = useTransactionStore();
+	const { categories } = useCategoryStore();
 	const [categoryTransactions, setCategoryTransactions] = useState<CategoryTransactions>();
 
 	useEffect(() => {
@@ -29,29 +31,42 @@ function Statistics() {
 			if (vals) {
 				map[record.category] = { ...vals, total: vals.total + record.amount };
 			} else {
+				const catagory = categories.find(item => (item.name === record.category));
+
 				map[record.category] = {
 					list: [record],
 					total: record.amount,
+					icon: catagory?.icon || 'circle',
 				};
 			}
 		});
 
 		// 排序...
 		setCategoryTransactions(map);
-	}, [transactions]);
+	}, [transactions, categories]);
 
 	const renderRecord = useMemo(() => {
 		if (!categoryTransactions) return null;
 
-		return Object.entries(categoryTransactions).map(([key, record]) => (
-			<View key={key}>
-				<Text>{key}</Text>
-				<Text>{record.total}</Text>
-			</View>
+		return Object.entries(categoryTransactions).map(([categoryName, record]) => (
+			<Surface key={categoryName} style={styles.recordItem}>
+				<Icon source={record.icon} size={24} />
+				<Text style={styles.categoryName}>{categoryName}</Text>
+				<View style={styles.recordDetails}>
+					<Text>总金额: ¥{record.total}</Text>
+					{/* <Text>交易笔数: {record.count}</Text> */}
+				</View>
+			</Surface>
 		));
 	}, [categoryTransactions]);
 
-	return <Surface style={styles.container}>{renderRecord}</Surface>;
+	return (
+		<View style={styles.container}>
+			<ScrollView>
+				<View style={styles.record}>{renderRecord}</View>
+			</ScrollView>
+		</View>
+	);
 }
 
 export default Statistics;
@@ -59,5 +74,22 @@ export default Statistics;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	record: {
+		gap: 12,
+		margin: 12,
+	},
+	recordItem: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 12,
+		borderRadius: 8,
+	},
+	categoryName: {
+		fontSize: 14,
+	},
+	recordDetails: {
+		alignItems: 'flex-end',
 	},
 });
